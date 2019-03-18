24bf61c228321f3f594459104831ba43ef71165f
Krishna <krishna2121.rana@gmail.com>
2019-03-18 23:42:49 +0100

    Initial setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
+
 const App = () => {
-    return React.createElement(
-        "div", {},
-        React.createElement("h1", {}, "Adoptme")
-    );
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));

599d44d5bd6924756f99ac9ff10ab96783cdcb35
Krishna <krishna2121.rana@gmail.com>
2019-03-19 23:43:15 +0100

    custom react hook

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { ANIMALS } from "petfinder-client";
+import useDropdown from "./useDropdown.jsx";
 const SearchParams = () => {
   const [location, setLocation] = useState("Kathmandu");
-  const [animal, setAnimal] = useState("dog");
+  // const [animal, setAnimal] = useState("dog");
   const [breeds, setBreeds] = useState([]);
-  const [breed, setBreed] = useState("Havanes");
+  const [animal, AnimalDropdown] = useDropdown("Aninal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
+  // const [breed, setBreed] = useState("Havanes");
   return (
     <div>
       <form action="">
@@ -20,42 +24,10 @@ const SearchParams = () => {
         </label>
         <br />
         <br />
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={e => setAnimal(e.target.value)}
-            onBlur={e => setAnimal(e.target.value)}
-          >
-            <option value="" />
-            {ANIMALS.map(animal => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
         <br />
         <br />
-        <label htmlFor="breed">
-          breeds
-          <select
-            name=""
-            id="breed"
-            value={breed}
-            onChange={e => setBreed(e.target.value)}
-            onBlur={e => setBreed(e.target.value)}
-            disabled={!breeds.length}
-          >
-            <option />
-            {breeds.map(breed => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
+        <BreedDropdown />
         <br />
         <br />
         <button>Submit</button>
